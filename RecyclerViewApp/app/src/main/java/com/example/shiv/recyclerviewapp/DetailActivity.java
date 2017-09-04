8038e5ba916d811304448d834cb3b620409a0154
Shibajyoti Debbarma <debbarmaonit@gmail.com>
2017-09-04 19:08:34 +0530

    Initial Layout Changed

---
+++ RecyclerViewApp/app/src/main/java/com/example/shiv/recyclerviewapp/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.shiv.recyclerviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shiv on 30-08-2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+
+
+
+}
